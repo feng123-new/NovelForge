6e6dc0057bb8a48e6cd80dfad77cabe7a6e8da3f
feng123-new <3494617655@qq.com>
2026-09-01 13:40:58 +0800

    fix: serve readable embedded OpenAPI JSON

--- internal/server/openapi.go
+++ internal/server/openapi.go
@@ -1,6 +1,17 @@
 package server
 
-import _ "embed"
+import (
+	"bytes"
+	_ "embed"
+	"encoding/json"
+)
 
 //go:embed openapi.json
 var openAPISpec []byte
+
+func init() {
+	var formatted bytes.Buffer
+	if err := json.Indent(&formatted, openAPISpec, "", "  "); err == nil {
+		openAPISpec = append(formatted.Bytes(), '\n')
+	}
+}
